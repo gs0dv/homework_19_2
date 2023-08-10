68d5d7b48e1e87e2dc94255c3ad4a8bc2a983663
gs0dv <gs0dv@main.ru>
2023-08-10 20:09:00 +0300

    update files

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -1,5 +1,7 @@
 from django import template
 
+from config import settings
+
 register = template.Library()
 
 url_default_image = 'https://rublevbar.com/files/oldestate/image/no_product.jpg'
@@ -7,7 +9,7 @@ url_default_image = 'https://rublevbar.com/files/oldestate/image/no_product.jpg'
 
 def build_path(val):
     if val:
-        return f'/media/{val}'
+        return f'{settings.MEDIA_URL}{val}'
 
     return url_default_image
 
